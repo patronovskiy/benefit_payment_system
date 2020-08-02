610888a4b54e897bd386e75564d10886ba4b943b
Galina Martiushova (patronovskiy) <patronovskiy@mail.ru>
2020-08-02 12:07:17 +0500

    feat: put address ia a separate class Address

---
+++ src/edu/patronovskiy/studentorder/domain/Address.java
@@ -0,0 +1,54 @@
+package edu.patronovskiy.studentorder.domain;
+
+/**
+ * @author patronovskiy
+ * @link https://github.com/patronovskiy
+ */
+
+public class Address {
+    private String street;
+    private String building;
+    private String extension;   //№ корпуса дома
+    private String appartment;  //№ квартиры
+    private String postCode;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(final String street) {
+        this.street = street;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(final String building) {
+        this.building = building;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(final String extension) {
+        this.extension = extension;
+    }
+
+    public String getAppartment() {
+        return appartment;
+    }
+
+    public void setAppartment(final String appartment) {
+        this.appartment = appartment;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(final String postCode) {
+        this.postCode = postCode;
+    }
+}
